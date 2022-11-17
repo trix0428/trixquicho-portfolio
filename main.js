5031a14687670d781076b689a55e1f147df25763
Trix Quicho <58583653+trix0428@users.noreply.github.com>
2022-11-18 01:01:16 +0800

    Add files via upload
    - added hamburger menu

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navitems = document.querySelector(".nav-items");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navitems.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navitems.classList.remove("active");
+  })
+);
+
 // const navbarToggler = document.querySelector(".navbar-toggler");
 // const navbarMenu = document.querySelector(".side-navbar ul");
 
