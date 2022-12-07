d1a9075bab11d4eb4f89e1b783a7e1800f257c7e
Trix Quicho <58583653+trix0428@users.noreply.github.com>
2022-12-08 01:00:45 +0800

    Add files via upload
    - added typewriter effect
    - added navigation

--- main.js
+++ main.js
@@ -31,6 +31,65 @@ document.querySelectorAll(".nav-link").forEach((n) =>
 //   }
 // }
 
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = "";
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) {
+    delta /= 2;
+  }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === "") {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName("typewrite");
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute("data-type");
+    var period = elements[i].getAttribute("data-period");
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
+
 // SECOND SECTION - CLICKING NAV
 
 document.addEventListener("DOMContentLoaded", function (event) {
@@ -96,16 +155,16 @@ btn_hobbies.addEventListener("click", () => {
 
 // CONTACT
 
-function SendMail() {
-  var params = {
-    subject_id: document.getElementById("subject_id").value,
-    message_id: document.getElementById("message_id").value,
-    name_id: document.getElementById("name_id").value,
-    email_id: document.getElementById("email_id").value,
-  };
-  emailjs
-    .send("service_xyvk7um", "template_1gkv9zf", params)
-    .then(function (res) {
-      alert("Success!" + res.status);
-    });
-}
+// function SendMail() {
+//   var params = {
+//     subject_id: document.getElementById("subject_id").value,
+//     message_id: document.getElementById("message_id").value,
+//     name_id: document.getElementById("name_id").value,
+//     email_id: document.getElementById("email_id").value,
+//   };
+//   emailjs
+//     .send("service_xyvk7um", "template_1gkv9zf", params)
+//     .then(function (res) {
+//       alert("Success!" + res.status);
+//     });
+// }
